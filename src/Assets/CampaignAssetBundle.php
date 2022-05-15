8ae803fc21dfa912aac568870a6b8ccfa6fb247b
Igor Maliy <imaliy.filsh@gmail.com>
2022-05-15 14:48:25 +0300

    Development

---
+++ src/Assets/CampaignAssetBundle.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailery\Campaign\Assets;
+
+use Mailery\Web\Assets\VueAssetBundle;
+use Yiisoft\Assets\AssetBundle;
+
+class CampaignAssetBundle extends AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public ?string $basePath = '@public/assets/@maileryio/mailery-campaign-assets';
+
+    /**
+     * {@inheritdoc}
+     */
+    public ?string $baseUrl = '@assetsUrl/@maileryio/mailery-campaign-assets';
+
+    /**
+     * {@inheritdoc}
+     */
+    public ?string $sourcePath = '@npm/@maileryio/mailery-campaign-assets/dist';
+
+    /**
+     * {@inheritdoc}
+     */
+    public array $css = [
+        'main.min.css',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public array $js = [
+        'main.umd.min.js',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public array $depends = [
+        VueAssetBundle::class,
+    ];
+}
---
+++ src/Middleware/AssetBundleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mailery\Campaign\Middleware;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Mailery\Assets\AssetBundleRegistry;
+use Mailery\Campaign\Assets\CampaignAssetBundle;
+
+class AssetBundleMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param AssetBundleRegistry $assetBundleRegistry
+     */
+    public function __construct(
+        private AssetBundleRegistry $assetBundleRegistry
+    ) {}
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->assetBundleRegistry->add(CampaignAssetBundle::class);
+
+        return $handler->handle($request);
+    }
+}
